68d7693a42560c7fd197b0e143cd286b65108cdf
tho <tho.ha_2502@hcmut.edu.vn>
2020-06-28 17:16:56 +0700

    create sfcs-vue-native

---
+++ src/screens/home/introSlide.vue
@@ -0,0 +1,41 @@
+<template>
+  <nb-container :style="{ backgroundColor: '#fff' }">
+    <nb-content padder>
+      <nb-card>
+        <nb-card-item cardBody>
+          <image
+            :source="cardImage"
+            class="card-item-image"
+            :style="stylesObj.cardItemImage"
+          />
+        </nb-card-item>
+      </nb-card>
+    </nb-content>
+  </nb-container>
+</template>
+
+<script>
+import cardImage from "../../../assets/bklast1.png";
+export default {
+  data() {
+    return {
+      cardImage,
+      stylesObj: {
+        cardItemImage: {
+          resizeMode: "contain"
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.mb-15 {
+  margin-bottom: 15;
+}
+.card-item-image {
+  flex: 1;
+  height: 200;
+}
+</style>
---
+++ src/screens/home/tabCategory.vue
@@ -0,0 +1,33 @@
+<template>
+  <nb-container>
+    <nb-tabs :renderTabBar="getScollableTabComp">
+      <nb-tab heading="Tab1">
+        <tab-one />
+      </nb-tab>
+      <nb-tab heading="Tab2">
+        <tab-two />
+      </nb-tab>
+      <nb-tab heading="Tab3">
+        <tab-three />
+      </nb-tab>
+    </nb-tabs>
+  </nb-container>
+</template>
+
+<script>
+import React from "react";
+import { ScrollableTab } from "native-base";
+import TabOne from "./components/tabOne";
+import TabTwo from "./components/tabTwo";
+import TabThree from "./components/tabThree";
+
+export default {
+  components: { TabOne, TabTwo, TabThree},
+  methods: {
+    getScollableTabComp() {
+      return <ScrollableTab />;
+    }
+  }
+};
+
+</script>
